342be4e1d3fb033dd14a6fab7073f96cf9689ca3
luwg <luwg@zzlc.com>
2020-01-11 12:32:38 +0800

    test01

--- spring-study-new-lesson01/src/main/java/com/youyoustudio/Test01.java
+++ spring-study-new-lesson01/src/main/java/com/youyoustudio/Test01.java
@@ -1,4 +1,18 @@
 package com.youyoustudio;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test01 {
+    private ApplicationContext ioc = new ClassPathXmlApplicationContext("ioc01.xml");
+
+    @Test
+    public void test01(){
+        Student student = (Student)ioc.getBean("student");
+        System.out.println(student);
+
+    }
+
+
 }
