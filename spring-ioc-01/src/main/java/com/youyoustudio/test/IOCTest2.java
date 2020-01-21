4e15f984c398746c3498228077ae2337e712d27f
lbxx <476427808@qq.com>
2020-01-21 15:24:19 +0800

    临时提交

--- spring-ioc-01/src/main/java/com/youyoustudio/test/IOCTest2.java
+++ spring-ioc-01/src/main/java/com/youyoustudio/test/IOCTest2.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Properties;
+
 public class IOCTest2 {
     private ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ioc2.xml");
 
@@ -62,5 +64,40 @@ public class IOCTest2 {
     public void testPersonMap(){
         Person personMap = applicationContext.getBean("personMap", Person.class);
         System.out.println(personMap);
+    }
+
+    /**
+     * 正确为Properties赋值
+     */
+    @Test
+    public void testProperties(){
+        Person personProperties = applicationContext.getBean("personProperties", Person.class);
+        System.out.println(personProperties);
+        String property = personProperties.getProperties().getProperty("key01");
+        System.out.println(property);
+        Properties properties = new Properties();
+    }
+
+    /**
+     * 测试util名称空间
+     */
+    @Test
+    public void testUtilNamespace(){
+        Person personUtil = applicationContext.getBean("personUtil", Person.class);
+        System.out.println(personUtil);
+        Object mapTest = applicationContext.getBean("mapTest");
+        System.out.println(mapTest);
+        System.out.println(mapTest.getClass());//class java.util.LinkedHashMap
+    }
+
+    /**
+     * 测试级联属性
+     */
+    @Test
+    public void testCascade(){
+        Person personCascade = applicationContext.getBean("personCascade", Person.class);
+        System.out.println(personCascade);
+        Car car = applicationContext.getBean("car", Car.class);
+        System.out.println(car.getPrice());
     }
 }
